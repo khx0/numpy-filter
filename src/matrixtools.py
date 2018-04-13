32a747a4ae54ba5c62e118af78a3e18cf6484b4a
Nikolas Schnellbächer <khx0@posteo.net>
2018-04-13 08:58:28 +0200

    completed symmetricMatrix readme

--- src/matrixtools.py
+++ src/matrixtools.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2018-04-12
+# date: 2018-04-13
 # file: matrixtools.py
 ##########################################################################################
 
@@ -11,17 +11,18 @@ import numpy as np
 
 def fillSymmetricMatrix(X):
     '''
-    This routine assumes, that the input matrix/array is upper triangular
-    with a filled diagonal like indicated below for 5x5 example
+    fillSymmetricMatrix routine
+    This routine assumes that the input matrix/array is upper triangular
+    with a filled diagonal like indicated below for a 5x5 example
     * * * * *           * * * * *
     - * * * *           * * * * *
     - - * * *   ===>    * * * * *
     - - - * *           * * * * *
     - - - - *           * * * * *
-    where the "-" indicate empyt (non filled) values and the asteriles (*) are the filled
-    values.  Then the fillSymmetrixMatrix() function will return the symmetic matrix
-    where the diagonal is preserved and the lower triangular is filled by their 
-    corresponding upper triangular values.
+    where the "-" symbols indicate empty (non filled) values and the asteriskes "*" 
+    symbols indicate filled values. Then the fillSymmetrixMatrix() function will return 
+    the symmetic matrix where the diagonal is preserved and the lower triangular 
+    is filled by their corresponding upper triangular values.
     '''
     assert X.shape[0] == X.shape[1], "Error: X is not quadratic."
     tmp = np.zeros_like(X)
