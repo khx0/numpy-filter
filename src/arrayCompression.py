0ae5362358c5d80febc55253c823d1d002a5010d
Nikolas Schnellbächer <khx0@posteo.net>
2019-01-17 23:16:54 +0100

    added arrayCompression module

---
+++ src/arrayCompression.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2019-01-17
+# file: arrayCompression.py
+##########################################################################################
+
+import os
+import sys
+import numpy as np
+
+def getIntervals(X):
+
+    pass
+    
+    return None
+
+if __name__ == '__main__':
+
+    pass
