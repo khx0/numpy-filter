22d7edee252143e2d534350ab274063e60775d66
Nikolas Schnellbächer <khx0@posteo.net>
2019-10-27 22:47:42 +0100

    added new test cases2

--- src/test_filter_getDisjointIntervals.py
+++ src/test_filter_getDisjointIntervals.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-10-26
+# date: 2019-10-27
 # file: test_filter_getDisjointIntervals.py
 # tested with python 2.7.15
 # tested with python 3.7.2
@@ -23,20 +23,47 @@ class TestGetDisjointIntervals(unittest.TestCase):
 
     def test_01(self):
 
+        X = [0, 1, 0, 1]
+        res = getDisjointIntervals(X)
+        self.assertTrue(res == [(1, 1, 1), (3, 3, 1)])
+
+        X = [1, 0, 1, 0]
+        res = getDisjointIntervals(X)
+        self.assertTrue(res == [(0, 0, 1), (2, 2, 1)])
+
     	X = [0, 0, 1, 1, 1]
     	res = getDisjointIntervals(X)
-    	print(res)
     	self.assertTrue(res == [(2, 4, 1)])
 
     	X = [0, 0, 1, 1, 1, 0, 1]
     	res = getDisjointIntervals(X)
-    	print(res)
     	self.assertTrue(res == [(2, 4, 1), (6, 6, 1)])
 
-    	X = [0]
-    	res = getDisjointIntervals(X)
-    	print(res)
-    	self.assertTrue(res == [])
+    def test_02(self):
+
+        X = []
+        res = getDisjointIntervals(X)
+        self.assertTrue(res == [])
+
+        X = [0]
+        res = getDisjointIntervals(X)
+        self.assertTrue(res == [])
+
+        X = [0, 0]
+        res = getDisjointIntervals(X)
+        self.assertTrue(res == [])
+
+        X = [0, 0, 0]
+        res = getDisjointIntervals(X)
+        self.assertTrue(res == [])
+
+        X = [0, 0, 0, 0]
+        res = getDisjointIntervals(X)
+        self.assertTrue(res == [])
+
+        X = [0, 0, 0, 0, 0]
+        res = getDisjointIntervals(X)
+        self.assertTrue(res == [])
 
 if __name__ == '__main__':
 
